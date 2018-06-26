0a0c6278de2eb1e25d44c9b3d8cb9150fa05649a
Safwan Bin Shahjehan <safwanshah.work@gmail.com>
2018-06-26 22:57:25 +0800

    first commit

---
+++ Assets/scripts/Playercollision.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Playercollision : MonoBehaviour {
+
+
+    public playermovement movement;
+
+
+
+    void OnCollisionEnter (Collision collisioninfo )
+    {
+       if (collisioninfo.collider.tag == "obstacle")
+        {
+
+            movement.enabled = false;
+
+        }
+        
+
+
+
+    }
+}
---
+++ Assets/scripts/playermovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class playermovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float forwardforce = 1400f;
+    public float sidewaysforce = 700f;
+	
+	// This is marked as "fixed"update because we use it
+    // to mess with physics
+	void FixedUpdate ()
+    {
+            //add a forward force
+          rb.AddForce(0, 0, forwardforce * Time.deltaTime);
+
+        if( Input.GetKey("d") )
+
+        {
+            rb.AddForce(sidewaysforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+
+        if (Input.GetKey("a"))
+
+        {
+            rb.AddForce(-sidewaysforce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+
+
+    }
+}
